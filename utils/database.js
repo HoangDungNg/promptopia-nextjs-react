54bd01dd65b168c8a953184676b635cd1944385f
Hoang Dung Nguyen <dungnguyen322003@gmail.com>
2023-08-13 23:29:30 -0400

    edit env.example, add description for database.js

--- utils/database.js
+++ utils/database.js
@@ -13,8 +13,8 @@ export const connectToDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: 'share_prompt',
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useNewUrlParser: true, // allow users to fall back to the old parser if they find a bug in the new parser
+      useUnifiedTopology: true, //  using the MongoDB driver's new connection management engine
     });
     isConnected = true;
     console.log('MongoDB connected');
